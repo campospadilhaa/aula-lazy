e7dfacb46fab3b2346348fd9f7c1301f5f0fd111
Anderson Campos Padilha <campospadilha.a@gmail.com>
2025-02-26 18:20:32 -0300

    Criação do projeto para testess de performance utilizando JPA

---
+++ src/main/java/com/campospadilha/a/aula_lazy/AulaLazyApplication.java
@@ -0,0 +1,13 @@
+package com.campospadilha.a.aula_lazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaLazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaLazyApplication.class, args);
+	}
+
+}
